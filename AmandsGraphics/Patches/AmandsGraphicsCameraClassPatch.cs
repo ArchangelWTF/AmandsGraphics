4d2e07aed83a2ac60fc6013a3645fa1bcef1b1df
Archangel <jesse@archangel.wtf>
2026-06-07 03:28:58 +0200

    Begin move to SPT 4.0
    - Begin code cleanup
    - Migrate solution and csproj to new format
    - Migrate target framework to netstandard2.1
    - Begin moving patches to .Patches namespace
    - Bring in various of my templating stuff (.gitignore & .editorconfig) as well as refs used in my other mods

---
+++ AmandsGraphics/Patches/AmandsGraphicsCameraClassPatch.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using SPT.Reflection.Patching;
+
+namespace AmandsGraphics.Patches;
+
+public sealed class AmandsGraphicsCameraClassPatch : ModulePatch
+{
+    protected override MethodBase GetTargetMethod()
+    {
+        return typeof(CameraClass).GetMethod(nameof(CameraClass.Blur));
+    }
+
+    [PatchPrefix]
+    public static bool PatchPrefix(ref CameraClass __instance, bool isActive, float time)
+    {
+        AmandsGraphicsClass.CameraClassBlur = isActive;
+
+        if (!isActive && __instance.IsActive)
+        {
+            return true;
+        }
+
+        return AmandsGraphicsPlugin.UIDepthOfField.Value == EUIDepthOfField.Off;
+    }
+}
